519dd68322298f90c3452ac6bfd8ece5b68c452c
Giovani Souza <shark.tec@hotmail.com>
2020-02-28 17:56:03 -0300

    end first phase

--- app/src/Controllers/UsersController.php
+++ app/src/Controllers/UsersController.php
@@ -2,40 +2,12 @@
 
 namespace App\Controllers;
 
-use App\Models\Users;
+use PM\Framework\CrudController;
 
-class UsersController
+class UsersController extends CrudController
 {   
-    public function index($container, $request)
+    protected function getModel() :string
     {
-        $user = new Users($container);       
-        return $user->all(); 
-    }
-    
-    public function show($container, $request)
-    {
-        $user = new Users($container);     
-        $id = $request->attributes->get(1);
-        return $user->get(['id' => $id]);
-    }
-    
-    public function create($container, $request)
-    {
-        $user = new Users($container);   
-        return $user->create($request->request->all()); 
-    }
-
-    public function update($container, $request)
-    {
-        $user = new Users($container);          
-        $id = $request->attributes->get(1);   
-        return $user->update(['id' => $id], $request->request->all());
-    }
-
-    public function delete($container, $request)
-    {
-        $user = new Users($container);     
-        $id = $request->attributes->get(1);
-        return $user->delete(['id' => $id]);
+        return "users_model";
     }
 }
